50e15f5c4431dea0e608664d2f167723f3eaeaf0
gadd <for.gadd@gmail.com>
2016-08-29 22:39:30 +0300

    Initial commit. This code based on official libGDX tutorials https://github.com/libgdx/libgdx/wiki/A-simple-game https://github.com/libgdx/libgdx/wiki/Extending-the-simple-game

---
+++ android/src/com/github/gaddsky/extradrop/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.gaddsky.extradrop;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.gaddsky.extradrop.ExtraDrop;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ExtraDrop(), config);
+	}
+}
---
+++ html/src/com/github/gaddsky/extradrop/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.github.gaddsky.extradrop.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.github.gaddsky.extradrop.ExtraDrop;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800, 480);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new ExtraDrop();
+        }
+}
