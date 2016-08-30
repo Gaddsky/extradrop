09362a5b5bd12ee08297bd5bf55aed6bd9c0d2da
gadd <for.gadd@gmail.com>
2016-08-30 14:56:17 +0300

    Changed game's name in main menu

--- core/src/com/github/gaddsky/extradrop/MainMenuScreen.java
+++ core/src/com/github/gaddsky/extradrop/MainMenuScreen.java
@@ -26,7 +26,7 @@ public class MainMenuScreen implements Screen {
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
-        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
+        game.font.draw(game.batch, "Welcome to ExtraDrop!!! ", 100, 150);
         game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
         game.batch.end();
 
